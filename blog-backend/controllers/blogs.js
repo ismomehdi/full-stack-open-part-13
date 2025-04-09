50be728b8d09b41bc53d325178035bf3c855380f
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-04-09 21:04:56 +0300

    ex 13.6

--- blog-backend/controllers/blogs.js
+++ blog-backend/controllers/blogs.js
@@ -1,6 +1,11 @@
 const router = require('express').Router()
 const { Blog } = require('../models')
 
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
 router.get('/', async (req, res) => {
 	const blogs = await Blog.findAll()
 	res.json(blogs)
@@ -10,6 +15,18 @@ router.post('/', async (req, res) => {
 	try {
 		const blog = await Blog.create(req.body)
 		res.json(blog)
+	} catch (error) {
+		return res.status(400).json({ error })
+	}
+})
+
+router.put('/:id', blogFinder, async (req, res) => {
+	if (!req.blog) res.status(404).end()
+
+	try {
+		req.blog.likes = req.body.likes
+		await req.blog.save()
+		res.json(req.blog)
 	} catch (error) {
 		return res.status(400).json({ error })
 	}

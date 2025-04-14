a542977689157bc126f4ab5468802d82a1ef29ad
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-04-14 16:53:14 +0300

    e 13.16

--- blog-backend/index.js
+++ blog-backend/index.js
@@ -1,24 +1,26 @@
-const express = require('express')
-const { PORT } = require('./util/config')
-const { connectToDatabase } = require('./util/db')
+const express = require("express");
+const { PORT } = require("./util/config");
+const { connectToDatabase } = require("./util/db");
 
-const app = express()
+const app = express();
 
-const blogsRouter = require('./controllers/blogs')
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
+const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const authorsRouter = require("./controllers/authors");
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/blogs', blogsRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
+app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+app.use("/api/authors", authorsRouter);
 
 const start = async () => {
-  await connectToDatabase()
+  await connectToDatabase();
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
-}
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-start()
+start();

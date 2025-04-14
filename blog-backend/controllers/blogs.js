5b24b1ad7d18a253d9d594a0101bbf4a28fd6688
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-04-14 15:26:37 +0300

    ex 13.10

--- blog-backend/controllers/blogs.js
+++ blog-backend/controllers/blogs.js
@@ -1,15 +1,34 @@
-const router = require('express').Router()
-const { Blog } = require('../models')
+const router = require("express").Router();
+const { Blog, User } = require("../models");
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("../util/config");
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      console.log(authorization.substring(7));
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch (error) {
+      console.log(error);
+      return res.status(401).json({ error: "token invalid" });
+    }
+  } else {
+    return res.status(401).json({ error: "token missing" });
+  }
+  console.log("token ok");
+  next();
+};
 
 const blogFinder = async (req, res, next) => {
-  req.blog = await Blog.findByPk(req.params.id)
-  next()
-}
+  req.blog = await Blog.findByPk(req.params.id);
+  next();
+};
 
-router.get('/', async (req, res) => {
-	const blogs = await Blog.findAll()
-	res.json(blogs)
-})
+router.get("/", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
 
 // "Starting with Express 5, route handlers and middleware
 // that return a Promise will call next(value) automatically
@@ -18,21 +37,22 @@ router.get('/', async (req, res) => {
 // This means that error handling middleware is not needed here.
 //
 // https://expressjs.com/en/guide/error-handling.html
-router.post('/', async (req, res) => {
-	const blog = await Blog.create(req.body)
-	res.json(blog)
-})
+router.post("/", tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id);
+  const blog = await Blog.create({ ...req.body, userId: user.id });
+  res.json(blog);
+});
 
-router.put('/:id', blogFinder, async (req, res) => {
-	if (!req.blog) res.status(404).end()
-	req.blog.likes = req.body.likes
-	await req.blog.save()
-	res.json(req.blog)
-})
+router.put("/:id", blogFinder, async (req, res) => {
+  if (!req.blog) res.status(404).end();
+  req.blog.likes = req.body.likes;
+  await req.blog.save();
+  res.json(req.blog);
+});
 
-router.delete('/:id', async (req, res) => {
-	await Blog.destroy({ where: { id: req.params.id } })
-	res.status(204).end()
-})
+router.delete("/:id", async (req, res) => {
+  await Blog.destroy({ where: { id: req.params.id } });
+  res.status(204).end();
+});
 
-module.exports = router
+module.exports = router;
